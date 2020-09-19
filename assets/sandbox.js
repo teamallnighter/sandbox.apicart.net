014b6a19d71d331f4beb52bf4831f302d5af850e
Chris <me@chrisconnelly.dev>
2020-09-19 16:45:25 -0600

    fix

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -1,31 +1,31 @@
 Apicart
     .setDevEnv()
     .configure({
-            store: new Apicart.Store({
-                token: "cTlIn5?3ODMsXifEpUvbmuY_Moqbsj?5NrHOYInYiSdptTxdGVR_f8ExfWj9qLsL",
-            }),
-            payments: new Apicart.Payments({
-                token: "Qsc5oKZix7Jdzpz8c0AkbqHNjlpMgW_naATAwJD4lNW6V0WyH!5cWcMhQiY1AHZI",
-            }),
-            vueComponents: {
-                paymentMethodsList: {
-                    allowedMethods: ["cash", "bank-transfer"],
-                },
-                cartDropdown: {
-                    dropdownPosition: "right",
-                },
-                category: {
-                    products: {
-                        list: {
-                            budder: [{
+        store: new Apicart.Store({
+            token: "cTlIn5?3ODMsXifEpUvbmuY_Moqbsj?5NrHOYInYiSdptTxdGVR_f8ExfWj9qLsL",
+        }),
+        payments: new Apicart.Payments({
+            token: "Qsc5oKZix7Jdzpz8c0AkbqHNjlpMgW_naATAwJD4lNW6V0WyH!5cWcMhQiY1AHZI",
+        }),
+        vueComponents: {
+            paymentMethodsList: {
+                allowedMethods: ["cash", "bank-transfer"],
+            },
+            cartDropdown: {
+                dropdownPosition: "right",
+            },
+            category: {
+                products: {
+                    list: {
+                        budder: {
+                            new: [{
                                 dataUrl: "https://cdn.apicart.dev/external/vyqhp5m7fb2rgrcpocn1rzezr6pzsg88/data/DM/DME0002.json",
                                 pageUrl: "/papaya-sorbet/",
-                            }, ],
-                            shatter: [{
+                            }],
+                            shatter: {
                                 dataUrl: "https://cdn.apicart.dev/external/vyqhp5m7fb2rgrcpocn1rzezr6pzsg88/data/DM/DME0001.json",
                                 pageUrl: "/legendary-haze/",
-
-                            }, ],
+                            },
                         },
                     },
                 },
