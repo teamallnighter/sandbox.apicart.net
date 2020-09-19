83a851c8fdae0c5ffdd37e53e597266629932b26
Chris <me@chrisconnelly.dev>
2020-09-19 16:28:42 -0600

    INIT

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -1,61 +1,63 @@
 Apicart
-	.setDevEnv()
-	.configure({
-		store: new Apicart.Store({ token: '9mCu3DlBCa4REI?Q7kKly!Rw6!_FvD8K_dgPXe1b20?r6!sPTQMyCpq_ADt!jXOD' }),
-		payments: new Apicart.Payments({ token: 'Q84lNQyLl?nBGvKxxbcdc!nWFKEZrK?L_Is2r9IaOJo14ONbXw1SMlPIeptcaFza' }),
-		vueComponents: {
-			paymentMethodsList: {
-				allowedMethods: ['cash', 'bank-transfer']
-			},
-			cartDropdown: {
-				dropdownPosition: 'right'
-			},
-			category: {
-				products: {
-					list: {
-						clothes: {
-							new: [
-								{
-									dataUrl: 'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/01/1.json',
-									pageUrl: '/green-t-shirt/'
-								},
-								'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/02/2.json',
-								'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/03/3.json',
-								'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/04/4.json',
-
-							],
-							discount: [
-								'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/04/4.json',
-								'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/03/3.json',
-								'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/02/2.json',
-								'https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/01/1.json',
-							]
-						}
-					}
-				}
-			}
-		},
-		vueComponentsTranslator: {
-			localization: {
-				en: {
-					categories: {
-						clothes: {
-							title: 'The most beautiful clothes',
-							description: "The most beautiful t-shirts.",
-							menu: 'T-shirts',
-							new: {
-								title: 'New collection',
-								description: "T-shirts from our new collection.",
-								menu: 'New collection'
-							},
-							discount: {
-								title: 'Discounted t-shirts',
-								description: "The most popular T-shirts for half price",
-								menu: 'Discount'
-							}
-						}
-					}
-				}
-			}
-		}
-	}).initVueBundle();
+    .setDevEnv()
+    .configure({
+        store: new Apicart.Store({
+            token: "cTlIn5?3ODMsXifEpUvbmuY_Moqbsj?5NrHOYInYiSdptTxdGVR_f8ExfWj9qLsL",
+        }, ),
+        payments: new Apicart.Payments({
+            token: "Qsc5oKZix7Jdzpz8c0AkbqHNjlpMgW_naATAwJD4lNW6V0WyH!5cWcMhQiY1AHZI",
+        }, ),
+        vueComponents: {
+            paymentMethodsList: {
+                allowedMethods: ["cash", "bank-transfer"],
+            },
+            cartDropdown: {
+                dropdownPosition: "right",
+            },
+            category: {
+                products: {
+                    list: {
+                        clothes: {
+                            new: [{
+                                    dataUrl: "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/01/1.json",
+                                    pageUrl: "/green-t-shirt/",
+                                },
+                                "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/02/2.json",
+                                "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/03/3.json",
+                                "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/04/4.json",
+                            ],
+                            discount: [
+                                "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/04/4.json",
+                                "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/03/3.json",
+                                "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/02/2.json",
+                                "https://cdn.apicart.dev/external/wlhv1egho2u4p0e0nkne2mks7f9btigi/data/01/1.json",
+                            ],
+                        },
+                    },
+                },
+            },
+        },
+        vueComponentsTranslator: {
+            localization: {
+                en: {
+                    categories: {
+                        clothes: {
+                            title: "The most beautiful clothes",
+                            description: "The most beautiful t-shirts.",
+                            menu: "T-shirts",
+                            new: {
+                                title: "New collection",
+                                description: "T-shirts from our new collection.",
+                                menu: "New collection",
+                            },
+                            discount: {
+                                title: "Discounted t-shirts",
+                                description: "The most popular T-shirts for half price",
+                                menu: "Discount",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }).initVueBundle();
